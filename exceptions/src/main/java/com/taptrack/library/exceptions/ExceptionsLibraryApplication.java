bd3a075cb3ed470d8bec0945394cb47864295abc
JuhMaran <julianemaran@gmail.com>
2025-10-14 13:46:12 -0300

    Adiciona biblioteca de exceptions

---
+++ exceptions/src/main/java/com/taptrack/library/exceptions/ExceptionsLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.taptrack.library.exceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExceptionsLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExceptionsLibraryApplication.class, args);
+	}
+
+}
